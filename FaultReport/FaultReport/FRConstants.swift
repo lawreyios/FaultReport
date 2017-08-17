537782e780df72253adb2e87f435a349ef8bba8e
Lawrence Tan <lawrence.tan@2359media.com>
2017-08-17 23:15:27 +0800

    Setup basic test and display Login View as root view

---
+++ FaultReport/FaultReport/FRConstants.swift
@@ -0,0 +1,9 @@
+//
+//  FRConstants.swift
+//  FaultReport
+//
+//  Created by Lawrence Tan on 17/8/17.
+//  Copyright © 2017 lawrey. All rights reserved.
+//
+
+import Foundation
---
+++ FaultReport/FaultReport/FRViewControllerFactory.swift
@@ -0,0 +1,13 @@
+//
+//  FRViewControllerFactory.swift
+//  FaultReport
+//
+//  Created by Lawrence Tan on 17/8/17.
+//  Copyright © 2017 lawrey. All rights reserved.
+//
+
+import UIKit
+
+class FRViewControllerFactory: NSObject {
+
+}
